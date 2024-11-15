58400718c4a38074347efe625ff161cbab672aca
bijay-05 <hawkeyesniper69@gmail.com>
2024-11-15 22:27:12 +0545

    feat: module to set up network security for vm

---
+++ azure/vm_network_security/main.tf
@@ -0,0 +1,29 @@
+resource "azurerm_network_security_group" "testnsg" {
+  name                = var.nsg_name
+  resource_group_name = var.rg_name
+  location            = var.rg_location
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_network_security_rule" "testnsr" {
+  name                        = var.nsr_name
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Ssh"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.rg_name
+  network_security_group_name = azurerm_network_security_group.testnsg.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "testsnsga" {
+  subnet_id                 = var.subnet_id
+  network_security_group_id = azurerm_network_security_group.testnsg.id
+}
+
---
+++ azure/vm_network_security/variables.tf
@@ -0,0 +1,15 @@
+variable rg_name {
+    type = string
+    default = "testrg"
+}
+
+variable rg_location {
+    type = string
+    default = "us-east-1"
+}
+
+variable subnet_id {
+    type = string
+    default = "fdsfds"
+}
+
