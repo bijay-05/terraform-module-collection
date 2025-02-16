4b7aaa95389ff037e42524402b5065733781e879
bijay-05 <hawkeyesniper69@gmail.com>
2025-02-16 19:36:39 +0545

    added for vm network interface with no public ip

---
+++ azure/plain_vm_network_interface/main.tf
@@ -0,0 +1,15 @@
+resource "azurerm_network_interface" "testnic" {
+  name                = var.nic_name
+  location            = var.rg_location
+  resource_group_name = var.rg_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
---
+++ azure/plain_vm_network_interface/variables.tf
@@ -0,0 +1,19 @@
+variable rg_name {
+    type = string
+    default = "testrg"
+}
+
+variable rg_location {
+    type = string
+    default = "us-east-1"
+}
+
+variable nic_name {
+    type = string
+    default = "testnic"
+}
+
+variable subnet_id {
+    type = string
+    default = "fewf"
+}
