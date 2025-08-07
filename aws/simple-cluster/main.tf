71d449b3bf24c2638b3bb6be71e524314bd89c4e
bijay-05 <hawkeyesniper69@gmail.com>
2025-08-07 23:02:44 +0545

    feat: add vpc endpoint for ec2 to private ssm access

--- aws/simple-cluster/main.tf
+++ aws/simple-cluster/main.tf
@@ -1,3 +1,17 @@
+locals {
+  services = {
+    "ec2messages" : {
+      "name" : "com.amazonaws.${var.region}.ec2messages"
+    },
+    "ssm" : {
+      "name" : "com.amazonaws.${var.region}.ssm"
+    },
+    "ssmmessages" : {
+      "name" : "com.amazonaws.${var.region}.ssmmessages"
+    }
+  }
+}
+
 resource "aws_vpc" "testvpc" {
     cidr_block = var.vpc_cidr
     enable_dns_support = true
@@ -43,4 +57,57 @@ resource "aws_route_table_association" "testroutetableassociation" {
     subnet_id = aws_subnet.testsubnet.id
     route_table_id = aws_route_table.testroutetable.id
 }
+
+resource "aws_iam_role" "ssm_role" {
+    name = "TestSSMRole"
+
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17",
+        Statement = [
+        {
+            Effect = "Allow",
+            Principal = {
+            Service = "ec2.amazonaws.com"
+            },
+            Action = "sts:AssumeRole"
+        }
+        ]
+    })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_attach" {
+    role       = aws_iam_role.ssm_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ssm_profile" {
+    name = "TestSSMInstanceProfile"
+    role = aws_iam_role.ssm_role.name
+}
+
+resource "aws_instance" "private" {
+    ami                    = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI
+    instance_type          = "t2.micro"
+    subnet_id              = aws_subnet.testsubnet.id
+    associate_public_ip_address = false
+    key_name               = "my-key-pair"
+
+    iam_instance_profile = aws_iam_instance_profile.ssm_profile.name
+
+    tags = {
+        environment = "test"
+    }
+}
+
+resource "aws_vpc_endpoint" "ssm_endpoint" {
+  for_each = local.services
+  vpc_id   = aws_vpc.testvpc.id
+  service_name        = each.value.name
+  vpc_endpoint_type   = "Interface"
+  security_group_ids  = [aws_security_group.ssm_https.id]
+  private_dns_enabled = true
+  ip_address_type     = "ipv4"
+  subnet_ids          = [aws_subnet.testsubnet.id]
+}
+
 
--- aws/simple-cluster/variables.tf
+++ aws/simple-cluster/variables.tf
@@ -12,3 +12,8 @@ variable "availability_zone" {
   type = string
   default = "ap-south-1a"
 }
+
+variable "region" {
+  type = string
+  default = "ap-south-1"
+}
